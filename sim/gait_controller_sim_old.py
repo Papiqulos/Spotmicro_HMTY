fae57ded21bae4865f1b56040951e59165635a94
George Papoutsas <osfpgeopap1925@gmail.com>
2026-04-24 18:05:40 +0300

    file org

--- sim/gait_controller_sim_old.py
+++ sim/gait_controller_sim_old.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import numpy as np
 import core.bezier_curve_gen as bezier
 import sim.kinematics_old as kinematics_old
--- sim/kinematics_old.py
+++ sim/kinematics_old.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import numpy as np
 import math
 from tools.utils import to_homogenous
